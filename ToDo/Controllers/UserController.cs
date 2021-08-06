13f283355c4ec5f09be825cb7b4c0eae77ffaf84
shea cooke <SheaSCooke@gmail.com>
2021-08-06 12:08:02 -0700

    finished edit functionality

--- ToDo/Controllers/UserController.cs
+++ ToDo/Controllers/UserController.cs
@@ -59,6 +59,37 @@ namespace ToDo.Controllers
 
             return Redirect("/User/Index");
         }
+
+
+        [Route("Edit/{Id}")]
+        public IActionResult Edit(int Id)
+        {
+            ViewBag.edit = _context.ToDos.Find(Id);
+
+            AddToDoViewModel addToDoViewModel = new AddToDoViewModel();
+            
+
+            return View(addToDoViewModel);
+
+        }
+
+        [HttpPost]
+        [Route("Edit/{id}")]
+        public IActionResult Edit(AddToDoViewModel addToDoViewModel, int Id)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.ToDos.Where(x => x.Id == Id).ToList().ForEach(x => x.Description = addToDoViewModel.Description);
+                _context.ToDos.Where(x => x.Id == Id).ToList().ForEach(x => x.Notes = addToDoViewModel.Notes);
+                _context.ToDos.Where(x => x.Id == Id).ToList().ForEach(x => x.Priority = addToDoViewModel.Priority);
+
+                _context.SaveChanges();
+                return Redirect("/User/Index");
+            }
+
+            ViewBag.edit = _context.ToDos.Find(Id);
+            return View(addToDoViewModel);
+        }
         
 
 
